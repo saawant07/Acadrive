6a2d8d6d8ade067f141533deec06042cd33cd940
saawant07 <saawantgupta00@gmail.com>
2025-10-08 22:44:26 +0530

    Update API URL for production

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://acadrive.onrender.com";
 
 const uploadForm = document.getElementById("uploadForm");
 const searchForm = document.getElementById("searchForm");
